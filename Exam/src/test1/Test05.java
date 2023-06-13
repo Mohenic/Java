d76311c3e68c81f5387708b2ccfcd6b0facc9afe
syw077 <syw077@gmail.com>
2023-06-13 17:38:53 +0900

    연습문제

--- Exam/src/test1/Test05.java
+++ Exam/src/test1/Test05.java
@@ -5,22 +5,40 @@ import java.util.Scanner;
 /*
  * 날짜 : 2023/06/13
  * 이름 : 손영우
- * 내용 : Java 연산자 연습문제
+ * 내용 : Java 조건문 연습문제
  */
 
 public class Test05 {
 	public static void main(String[] args) {
 		
-		Scanner sc = Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("점수 입력 : ");
 		int score = sc.nextInt();
 		
 		System.out.println("입력한 점수는 " + score + "입니다.");
 		
-		switch(){
+		switch(score/10){
+		case 10:
+		case 9:
+			System.out.println("A 입니다.");
+			break;
+		case 8:
+			System.out.println("B 입니다.");
+			break;
+		case 7:
+			System.out.println("C 입니다.");
+			break;
+		case 6:
+			System.out.println("D 입니다.");
+			break;
+		default:
+			System.out.println("F 입니다.");
+			break;
 			
 		}
+		
+		sc.close();
 	}
 
 }
--- Exam/src/test1/Test06.java
+++ Exam/src/test1/Test06.java
@@ -1,5 +1,28 @@
 package test1;
 
+import java.util.Scanner;
+
+/*
+ * 날짜 : 2023/06/13
+ * 이름 : 손영우
+ * 내용 : Java 삼항연산자 연습문제
+ */
+
+
 public class Test06 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("x값 입력 : ");
+		int x = sc.nextInt();
+		
+		System.out.println("y값 입력 : ");
+		int y = sc.nextInt();
+		
+		String result = (x > y) ? "x가 더 큽니다." : "y가 더 큽니다.";
+		
+		System.out.println(result);
+	}
 
 }
