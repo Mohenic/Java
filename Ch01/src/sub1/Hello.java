ef008eda68df61952ec1c6bc58b8e1400b259ee2
syw077 <syw077@gmail.com>
2023-06-13 15:21:49 +0900

    Ch01 수업예제

---
+++ Ch01/src/sub1/Hello.java
@@ -0,0 +1,26 @@
+package sub1;
+
+/**
+ * 날짜 : 2023/06/12
+ * 이름 : 손영우
+ * 내용 : Ch01.Java 개발환경 구축
+ */
+public class Hello {
+
+	// 프로그램 진입점
+	public static void main(String[] args) {
+		
+		// 기본출력
+		System.out.println("Hello World~");
+		System.out.println("Hello Java~");
+		
+		// 한줄출력
+		System.out.print("Hello ");
+		System.out.print("Korea~");
+		
+		// 개행처리
+		System.out.print("\n");
+		System.out.print("Hello Busan~");
+		
+	}
+}
