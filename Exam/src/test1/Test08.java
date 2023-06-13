7d09c9ec74d2caf7c8a60185ee0076c1f73b630a
syw077 <syw077@gmail.com>
2023-06-13 17:40:18 +0900

    연습문제

--- Exam/src/test1/Test08.java
+++ Exam/src/test1/Test08.java
@@ -12,7 +12,7 @@ public class Test08 {
         
         for (int i = 0; i < n; i++) {
             for (int j = n - 1; j > i; j--) {
-                System.out.print(	" ");
+                System.out.print(" ");
             }
             for (int j = 0; j < 2 * i + 1; j++) {
                 System.out.print("*");
