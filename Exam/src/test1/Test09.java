413c8a93f05ee643d820908f58580b3de8409bf4
syw077 <syw077@gmail.com>
2023-06-13 17:46:44 +0900

    연숩문제

--- Exam/src/test1/Test09.java
+++ Exam/src/test1/Test09.java
@@ -1,5 +1,37 @@
 package test1;
 
+import java.util.Iterator;
+
+/*
+ * 날짜 : 2023/06/13
+ * 이름 : 손영우
+ * 내용 : 자바 반복문 연습문제
+ */
+
 public class Test09 {
+	public static void main(String[] args) {
+		
+		int count = 0;
+		for(int i=1 ; i<=9 ; i++) {
+			
+			if (i<=5) {
+				count++;
+				
+			} else {
+				count--;
+				
+			}
+			
+			for(int j=1 ; j<=5-count; j++) {
+				System.out.print(" ");
+			}
+			
+			for(int k=1 ; k<=2*count-1 ; k++) {
+				System.out.print("*");
+			}
+			
+			System.out.print("\n");
+		}
+	}
 
 }
