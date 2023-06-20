a91e88fb68cc9a1c78479fd563d21946e7787b84
syw077 <syw077@gmail.com>
2023-06-20 17:48:55 +0900

    추가

---
+++ TextBook/src/ch06/sec10/exam01/Calculaotr.java
@@ -0,0 +1,15 @@
+package ch06.sec10.exam01;
+
+public class Calculaotr {
+	
+	static double pi = 3.14159;
+	
+	static int plus(int x, int y) {
+		return x + y;
+	}
+	
+	static int minus(int x, int y) {
+		return x - y;
+	}
+
+}
---
+++ TextBook/src/ch06/sec10/exam01/CalculatorExample.java
@@ -0,0 +1,16 @@
+package ch06.sec10.exam01;
+
+public class CalculatorExample {
+	public static void main(String[] args) {
+		
+		double result1 = 10 * 10 * Calculaotr.pi;
+		int result2 = Calculaotr.plus(10, 5);
+		int result3 = Calculaotr.minus(10,5);
+		
+		System.out.println("result 1 : " + result1);
+		System.out.println("result 2 : " + result2);
+		System.out.println("result 3 : " + result3);
+		
+	}
+
+}
